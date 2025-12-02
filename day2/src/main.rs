81542f8a9edd30f9066b0ceddaf2fb57aa4006f6
dotconfig404 <dotconfig@krutt.org>
2025-12-02 20:45:00 +0100

    oh

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -54,29 +54,66 @@ fn main() {
         //state.put_id_range(id_start, id_end);
     }
 
-    // STATE PROCESSING
+    //// STATE PROCESSING
+    //for mut id_range in state.id_ranges {
+    //    //println!("{}, {}, {}", state.idx, id_range.start, id_range.end);
+    //    //let mut num = id_range.start;
+    //    for num in id_range.start..id_range.end+1 {
+    //        //println!("num: {}", num);
+    //        let num_len = num.to_string().len();
+    //        if num_len % 2 == 0 {
+    //            let mut s = num.to_string();
+    //            let (n1, n2) = s.split_at(num_len/2);
+    //            //println!("{n1}, {n2}");
+    //            if n1 == n2 {
+    //                //println!("Woops");
+    //                id_range.added += num;
+    //            }
+    //        }
+    //    }
+    //    state.total_added += id_range.added;
+    //    state.idx += 1;
+    //}
+
     for mut id_range in state.id_ranges {
         //println!("{}, {}, {}", state.idx, id_range.start, id_range.end);
         //let mut num = id_range.start;
         for num in id_range.start..id_range.end+1 {
-            //println!("num: {}", num);
             let num_len = num.to_string().len();
-            if num_len % 2 == 0 {
-                let mut s = num.to_string();
-                let (n1, n2) = s.split_at(num_len/2);
-                //println!("{n1}, {n2}");
-                if n1 == n2 {
-                    //println!("Woops");
-                    id_range.added += num;
+            for n in 1..num_len {
+                let mut num_str = num.to_string();
+                if num_len % n == 0 {
+                    let mut equal_parts = true;
+                    println!("num_str: {}, num_len {}, n {} ", num_str, num_len, n);
+                    let mut parts =  vec![];
+                    let mut part1 = "";
+                    let mut part2 = "";
+                    for _ in 0..(num_len/n)-1 {
+                        (part1, part2) = num_str.split_at(n);
+                        parts.push(part1);
+                    }
+                    parts.push(part2);
+                    let mut parts_it = parts.into_iter();
+                    let mut parts_it_clone = parts_it.clone();
+                    parts_it_clone.next();
+                    
+                    for part in parts_it_clone {
+                        if part != parts_it.next().unwrap() {
+                            equal_parts = false;
+                        }
+                    }
+                    if equal_parts {
+                        id_range.invalid_ids.push(num);
+                        id_range.added += num;
+                    }
                 }
             }
         }
         state.total_added += id_range.added;
         state.idx += 1;
     }
-
     println!("answer 1: {}", state.total_added);
-    //println!("answer 2: {}", state.total_zeroes);
+    println!("answer 2: {}", state.total_added);
 }
 
 

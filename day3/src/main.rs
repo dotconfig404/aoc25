a8bb9e4a05629fbb3191fe4b4f78921a0beace3f
dotconfig404 <dotconfig@krutt.org>
2025-12-04 22:01:57 +0100

    sth like that

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -16,13 +16,25 @@ fn main() {
 }
 
 fn get_max_joltage(bank_str: &str) -> u32 {
-    let max_joltage = 0;
+    let mut max_joltage = 0;
     let mut bank = get_bank(bank_str);
     sort_bank_by_joltage(&mut bank);
-    println!("Sorted: {} ",bank );
-    bank.batteries.sort_by_key(|k| k.pos);  
-    println!("Sorted: {} ",bank );
+    let highest = &bank.batteries[0];
+    let next_highest = &bank.batteries[1];
+    // if highest voltage is after the next max voltage = 
+    // next_highest'+'highest
+    if highest.pos > next_highest.pos {
+        max_joltage = combine_batteries_to_joltage(&highest, &next_highest);
+    }
+        
+    bank.batteries.sort_by_key(|k| k.pos);
     return max_joltage;
+}
+
+fn combine_batteries_to_joltage(bat1: &Battery, bat2: &Battery) -> u32 {
+    let mut j1_s = bat1.joltage.to_string();
+    let j2_s = bat2.joltage.to_string();
+    return j1_s.push(j2_s).parse().unwrap();
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -84,7 +96,7 @@ fn get_bank(bank_str: &str) -> Bank {
     };
     for (pos, joltage_c) in bank_str.chars().enumerate() {
         match joltage_c {
-            '0'|'1'|'2'|'3'|'4'|'5'|'6'|'7'|'8'|'9' => {
+            '0'..'9' => {
                 let battery = Battery{
                     pos: pos as u32,//u32::try_from(pos).unwrap(),
                     // int_from_ascii not 'stabilized' yet! it is merged, but seems we must tell

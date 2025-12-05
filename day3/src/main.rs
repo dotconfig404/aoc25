def3aef3d9e1efd47c6e976d375fb55e7b372e2b
dotconfig404 <dotconfig@krutt.org>
2025-12-05 17:42:54 +0100

    got dem batteries almost working

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,37 +4,50 @@ use std::fs;
 use std::cmp::Ordering;
 
 fn main() {
-    //let file_name = env::args().nth(1).unwrap(); 
-    //let content: Vec<u8> = fs::read(file_name).unwrap();
-    //let content_string: String = String::try_from(content).unwrap();
+    let file_name = env::args().nth(1).unwrap(); 
+    let content: Vec<u8> = fs::read(file_name).unwrap();
+    let content_string: String = String::try_from(content).unwrap();
 
     let example_bank = "818181911112111";
 
-    get_max_joltage(example_bank);
+    //let max_joltage = get_max_joltage(example_bank);
     //println!("answer 1: {}", state.total_added);
     //println!("answer 2: {}", state.total_added);
+    let mut max_joltage: u32 = 0;
+    let banks = content_string.split('\n');
+    for bank in banks {
+        println!("{bank}");
+        println!("max: {}", get_max_joltage(&bank));
+        max_joltage += get_max_joltage(&bank);
+    }
+    println!("max joltage: {}", max_joltage);
 }
 
 fn get_max_joltage(bank_str: &str) -> u32 {
     let mut max_joltage = 0;
     let mut bank = get_bank(bank_str);
-    sort_bank_by_joltage(&mut bank);
+
+    // reverse sorting by joltage -> highest joltage first
+    bank.batteries.sort_by(|a, b| b.joltage.cmp(&a.joltage));
     let highest = &bank.batteries[0];
-    let next_highest = &bank.batteries[1];
-    // if highest voltage is after the next max voltage = 
-    // next_highest'+'highest
-    if highest.pos > next_highest.pos {
-        max_joltage = combine_batteries_to_joltage(&highest, &next_highest);
-    }
+    println!("{bank}");
+    let mut bank2 = get_bank(bank_str);
+    bank.batteries = bank2.batteries.split_off(highest.pos as usize);
+    println!("{bank}");
+
+    // sorting new one
+    bank.batteries.sort_by(|a, b| b.joltage.cmp(&a.joltage));
+    
+    max_joltage = combine_batteries_to_joltage(&bank.batteries[0], &bank.batteries[1]);
         
-    bank.batteries.sort_by_key(|k| k.pos);
     return max_joltage;
 }
 
-fn combine_batteries_to_joltage(bat1: &Battery, bat2: &Battery) -> u32 {
-    let mut j1_s = bat1.joltage.to_string();
-    let j2_s = bat2.joltage.to_string();
-    return j1_s.push(j2_s).parse().unwrap();
+fn combine_batteries_to_joltage(left_bat: &Battery, right_bat: &Battery) -> u32 {
+    let mut l_jolt = left_bat.joltage.to_string();
+    let r_jolt = right_bat.joltage.to_string();
+    l_jolt.push_str(&r_jolt); 
+    return l_jolt.parse().unwrap();
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -42,48 +55,17 @@ struct Battery {
     pos: u32,
     joltage: u32,
 }
-impl Ord for Battery {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        if self.joltage < other.joltage {
-            Ordering::Less
-        } else if self.joltage > other.joltage {
-            Ordering::Greater
-        } else {
-            Ordering::Equal
-        }
-    }
-}
-
-impl PartialOrd for Battery{
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-impl PartialEq for Battery {
-    fn eq(&self, other: &Self) -> bool {
-        self.joltage == other.joltage
-    }
-}
-impl Eq for Battery {}
-
 
 struct Bank {
     batteries: Vec<Battery>,
 }
-//use std::fmt;
-//impl fmt::Display for Bank {
-//    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//        for b in self.batteries {
-//            write!(f, "joltage {} at {}", b.joltage, b.pos)
-//        }
-//    }
-//}
 use std::fmt;
 
 impl fmt::Display for Bank {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        //writeln!("Joltages");
         for b in &self.batteries {
-            writeln!(f, "joltage {} at {}", b.joltage, b.pos)?;
+            write!(f, "{}", b.joltage)?;
         }
         Ok(())
     }
@@ -96,7 +78,7 @@ fn get_bank(bank_str: &str) -> Bank {
     };
     for (pos, joltage_c) in bank_str.chars().enumerate() {
         match joltage_c {
-            '0'..'9' => {
+            '0'..='9' => {
                 let battery = Battery{
                     pos: pos as u32,//u32::try_from(pos).unwrap(),
                     // int_from_ascii not 'stabilized' yet! it is merged, but seems we must tell
@@ -106,17 +88,15 @@ fn get_bank(bank_str: &str) -> Bank {
                     // thx SO https://stackoverflow.com/questions/43983414/how-to-convert-a-rust-char-to-an-integer-so-that-1-becomes-1
                 };
                 bank.batteries.push(battery.clone());
-                println!("{joltage_c}");
             },
             _ => continue,
         }
     }
-    println!("{bank}");
     return bank;
 }
 
-fn sort_bank_by_joltage(bank: &mut Bank) {
-    bank.batteries.sort(); 
-    return;
-}
+//fn sort_bank_by_joltage(bank: &mut Bank) {
+//    bank.batteries.sort(); 
+//    return;
+//}
 
